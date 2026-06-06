e9bb8dcdf7c33dd74e5dd48d8ab990d01d61d422
leduochhnd <leduochhnd@gmail.com>
2026-06-06 08:46:08 +0000

    Update App.tsx

--- web-dashboard/src/App.tsx
+++ web-dashboard/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import AppRoutes from './routes/AppRoutes';
 import GlobalStyles from './styles/GlobalStyles';
-import MapPage from './pages/MapPage';
 
 const App: React.FC = () => {
     return (
         <>
             <GlobalStyles />
-            <MapPage />
+            <Router>
+                <AuthProvider>
+                    <AppRoutes />
+                </AuthProvider>
+            </Router>
         </>
     );
 };
